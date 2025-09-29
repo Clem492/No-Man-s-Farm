ba6e4b12d237e855d1196c946e35688e257c5f46
Turpin Clément <c.turpin@rubika-edu.re>
2025-09-29 16:29:08 +0400

    ajout du dossier script

---
+++ Assets/scripts/movement_player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class movement_player : MonoBehaviour
+{
+  [SerializeField]  float player_speed;
+    // fonction pour pouvoir déplacer le joueur 
+    void movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+
+
+}
