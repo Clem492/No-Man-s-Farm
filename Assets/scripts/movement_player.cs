cb929598783f0e8625b8dadc5f1726d41f6570e2
Turpin Clément <c.turpin@rubika-edu.re>
2025-09-29 17:03:46 +0400

    ajout premier deplacement

--- Assets/scripts/movement_player.cs
+++ Assets/scripts/movement_player.cs
@@ -8,21 +8,24 @@ public class movement_player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-
+            transform.Translate(0, 0, player_speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-
+            transform.Translate(0, 0, -(player_speed) * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.Translate(-(player_speed) * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(KeyCode.D))
         {
-
+            transform.Translate(player_speed * Time.deltaTime, 0, 0);
         }
     }
 
-
+    private void Update()
+    {
+        movement();
+    }
 }
