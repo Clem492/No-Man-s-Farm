33ceab2e690cb416d782d149947d825b9e76006a
Laurent Tristan <tl974.public@gmail.com>
2025-10-01 18:14:00 +0400

    affiche jour /nuit et numero vague

--- Assets/MyScripts/zombie/spawn_attaque_zombie.cs
+++ Assets/MyScripts/zombie/spawn_attaque_zombie.cs
@@ -5,8 +5,11 @@ public class spawn_attaque_zombie : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] GameObject zombie_prefabs;
+    [SerializeField] TMPro.TextMeshProUGUI jour_nuit;
+    [SerializeField] TMPro.TextMeshProUGUI vague;
     GameObject[] tab_zombie =new GameObject[1000];
     int nombre_zombie_spawn = 5;
+    int numero_vague;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,14 +26,17 @@ public class spawn_attaque_zombie : MonoBehaviour
         while (true)
         {
 
-
+            numero_vague += 1;
             // Attendre ... s avant de faire la suite de la fonction (jour)
             Debug.Log("jour");
+            jour_nuit.text = "jour";
             yield return new WaitForSeconds(10f);
 
 
             // Attendre ... s avant de faire la suite de la fonction (nuit)
+            jour_nuit.text = "nuit";
             Debug.Log("nuit");
+            vague.text = "vague : " + numero_vague;
             for (int i = 0; i < nombre_zombie_spawn; i++)
             {
                 tab_zombie[i] = Instantiate(zombie_prefabs,new Vector3(Random.Range(10,100),0.5f, Random.Range(10, 100)), Quaternion.identity);
