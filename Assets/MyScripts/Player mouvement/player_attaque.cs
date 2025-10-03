a81b081716fdf70c01509f1713be0bc5a0a9fd9c
Turpin Clément <c.turpin@rubika-edu.re>
2025-10-03 10:11:16 +0400

    ajout du script d'attaque

---
+++ Assets/MyScripts/Player mouvement/player_attaque.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class player_attaque : MonoBehaviour
+{
+
+
+
+    //fonction pour l'attaque du joueur 
+    void apply_attaque()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+
+        }
+    }
+    private void Update()
+    {
+        apply_attaque();
+    }
+
+}
