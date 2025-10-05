e98919d1142983490c1ab608480cb3c2dea6bd8b
Laurent Tristan <tl974.public@gmail.com>
2025-10-05 15:19:05 +0400

    pv zombie

---
+++ Assets/MyScripts/zombie/pv_zombie.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class pv_zombie : MonoBehaviour
+{
+    float nb_pv_zombie;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nb_pv_zombie = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void mort_zombie()
+    {
+        if (nb_pv_zombie <= 0) 
+            Destroy(gameObject);
+    }
+}
--- Assets/MyScripts/zombie/spawn_attaque_zombie.cs
+++ Assets/MyScripts/zombie/spawn_attaque_zombie.cs
@@ -12,7 +12,7 @@ public class spawn_attaque_zombie : MonoBehaviour
     int[] tab_pv_zombie = new int[1000];
     int nombre_zombie_spawn = 5;
     int numero_vague;
-    int pv_zombie = 3;
+    
     int depart = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -44,7 +44,7 @@ public class spawn_attaque_zombie : MonoBehaviour
             for (int i = depart; i < nombre_zombie_spawn; i++)
             {
                 tab_zombie[i] = Instantiate(zombie_prefabs,new Vector3(Random.Range(10,100),0.5f, Random.Range(10, 100)), Quaternion.identity);
-                tab_pv_zombie[i] = pv_zombie;
+                
                 depart += 1;
             }
             nombre_zombie_spawn += 2;
@@ -58,17 +58,5 @@ public class spawn_attaque_zombie : MonoBehaviour
         
            
     }
-    /*void zombie_elliminer ()
-    {
-        for (int i = 0; i < tab_pv_zombie.length; i++)
-        {
-            if (tab_pv_zombie[i] <= 0)
-            {
-                tab_pv_zombie.pop(i);
-                Destroy(tab_zombie[i])
-                tab_zombie.pop(i)
-                    depart -= 1;
-            }
-        }
-    }*/
+    
 }
