61c05f550bde1973039e93d9b77fc46c352d2c15
Laurent Tristan <tl974.public@gmail.com>
2025-10-29 21:21:05 +0400

    pv arbre mort

---
+++ Assets/MyScripts/Tree/pv_arbre_mort.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class pv_arbre_mort : MonoBehaviour
+{
+    public float nb_pv_arbre;
+    [SerializeField] GameObject branche;
+    Animator animator;
+
+    void Start()
+    {
+        nb_pv_arbre = 3;
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        destruction_arbre_mort();
+    }
+    public void perte_pv_arbre(float degats)
+    {
+        nb_pv_arbre -= degats;
+        animator.SetTrigger("degat_arbre");
+        
+    }
+    void destruction_arbre_mort()
+    {
+        if (nb_pv_arbre <= 0)
+        {
+            
+            for (int i = 0; i < Random.Range(2, 5); i++)
+            {
+                Instantiate(branche, new Vector3(gameObject.transform.position.x, 3, gameObject.transform.position.z), Quaternion.Euler(0, Random.Range(0, 360), 0));
+            }
+            Destroy(gameObject);
+        }
+           
+    }
+    
+}
