b539e8cfabb5f4187923de5d34278111489626db
Laurent Tristan <t.laurent@rubika-edu.re>
2025-10-30 12:59:58 +0400

    respawn player

--- Assets/MyScripts/Player/Player pv/pv_player.cs
+++ Assets/MyScripts/Player/Player pv/pv_player.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
-
+using System.Collections;
 public class pv_player : MonoBehaviour
 {
     float nb_pv_player;
+    bool est_mort;
     [SerializeField] TMPro.TextMeshProUGUI txt_pv_joueur;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //ont initialise les pv du joueur
         nb_pv_player = 10;
+        est_mort = false;
         //affiche pv joueur
         txt_pv_joueur.text = "PV : " + nb_pv_player;
     }
@@ -22,8 +24,25 @@ public class pv_player : MonoBehaviour
     void mort_player()
     {
         //si c'est point de vie sont a 0 ou en dessous de 0
-        if (nb_pv_player <= 0)
-            Destroy(gameObject);
+        if (nb_pv_player <= 0 && !est_mort)
+        {
+            
+            est_mort = true;
+            StartCoroutine(reset_pos());
+            
+        }
+            
+    }
+    IEnumerator reset_pos()
+    {
+        nb_pv_player = 10;
+        gameObject.GetComponent<CharacterController>().enabled = false;
+        gameObject.transform.position = new Vector3(250, 1, 250);
+        gameObject.GetComponent<CharacterController>().enabled = true;
+        txt_pv_joueur.text = "PV : " + nb_pv_player;
+        yield return new WaitForSeconds(1);
+    
+        est_mort = false;
     }
     //reduit les pv du joueur
     public void perte_pv_player(float degats)
