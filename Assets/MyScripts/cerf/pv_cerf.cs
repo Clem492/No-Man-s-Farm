1cecc1f8b350b7a23eca35702d6ce2e4f6995d9a
Laurent Tristan <t.laurent@rubika-edu.re>
2025-10-30 14:54:01 +0400

    zombie qui entre dans la maison

--- Assets/MyScripts/cerf/pv_cerf.cs
+++ Assets/MyScripts/cerf/pv_cerf.cs
@@ -21,7 +21,7 @@ public class pv_cerf : MonoBehaviour
         {
             for (int i = 0; i < Random.Range(2, 4); i++)
             {
-                Instantiate(viande, new Vector3(gameObject.transform.position.x, 2, gameObject.transform.position.z), Quaternion.Euler(0, Random.Range(0, 360), 0));
+                Instantiate(viande, new Vector3(gameObject.transform.position.x, 2.5f, gameObject.transform.position.z), Quaternion.Euler(0, Random.Range(0, 360), 0));
             }
             Destroy(gameObject);
         }
